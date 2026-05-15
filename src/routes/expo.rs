5f24e7499806c119cc9c26e7d751a82382155128
Amit niranjan <yamitniranjan1234@gmail.com>
2026-05-16 03:35:29 +0530

    I've completed all the requested Expo enhancements!

--- src/routes/expo.rs
+++ src/routes/expo.rs
@@ -1,7 +1,7 @@
 use crate::app_state::AppState;
 use crate::handlers::expo::{
     create_expo, edit_expo, get_all_expos, get_expo_details, get_expo_participants,
-    register_for_expo,
+    get_my_expos, register_for_expo,
 };
 use axum::{
     Router,
@@ -12,6 +12,7 @@ pub fn expo_routes() -> Router<AppState> {
     Router::new()
         .route("/api/expo", post(create_expo))
         .route("/api/expo", get(get_all_expos))
+        .route("/api/expo/mine", get(get_my_expos))
         .route("/api/expo/{expo_id}", get(get_expo_details).put(edit_expo))
         .route("/api/expo/{expo_id}/register", post(register_for_expo))
         .route(
